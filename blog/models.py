022fc5bea2503920bbd98930ffa14584edd6d8cb
L1ttle-Gem <ggotoeight@gmail.com>
2025-12-19 13:42:26 +0300

    Create blog app, first commit

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField(max_length=1000)
+    created_time = models.DateTimeField(default=timezone.now)
+    published_time = models.DateTimeField(blank=True, null=True)
+    
+    def publish(self):
+        self.published_time = timezone.now
+        self.save()
+    
+    def __str__(self):
+        return self.title    
